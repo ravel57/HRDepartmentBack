a4065b149a194139986036df69f42736617b6a3e
petr <petyalom@yandex.ru>
2020-11-16 00:36:04 +0300

    fix

--- build_frontend.ps1
+++ build_frontend.ps1
@@ -1,5 +1,5 @@
 cd ..\HRDepartmentFront
 git pull
-#yarn install
-#quasar build
-#Copy-Item -Path ".\dist\spa\*" -Destination "..\HRDepartamentBack\src\main\webapp\" -Recurse -Force -Exclude "index.html"
+yarn install
+quasar build
+Copy-Item -Path ".\dist\spa\*" -Destination "..\HRDepartamentBack\src\main\webapp\" -Recurse -Force -Exclude "index.html"
