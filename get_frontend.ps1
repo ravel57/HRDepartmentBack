cd4fcc8903c6d31b621f9b1c407ffaeec80250fe
petr <petyalom@yandex.ru>
2020-12-01 22:40:42 +0300

    Finished  get_frontend.ps1

--- get_frontend.ps1
+++ get_frontend.ps1
@@ -1,9 +1,16 @@
-cd ..\HRDepartmentFront
+$Path = Get-Location | Select -expand Path
+Get-ChildItem $Path\src\main\webapp -Exclude WEB-INF | Remove-Item -Recurse
+Set-Location ..\HRDepartmentFront
 git pull
 yarn install
 quasar build
-DEL / F / Q "..\HRDepartmentBack\src\main\webapp\css"
-DEL / F / Q "..\HRDepartmentBack\src\main\webapp\fonts"
-DEL / F / Q "..\HRDepartmentBack\src\main\webapp\icons"
-DEL / F / Q "..\HRDepartmentBack\src\main\webapp\js"
-Copy-Item -Path ".\dist\spa\*" -Destination "..\HRDepartamentBack\src\main\webapp\" -Recurse -Force -Exclude "index.html"
+Copy-Item -Path .\dist\spa\* -Destination $Path\src\main\webapp\ -Recurse -Force -Exclude index.html
+
+
+Set-Location  $Path\src\main\webapp\js
+Dir app.*.js | rename-item -newname app.js
+Dir vendor.*.js | rename-item -newname vendor.js
+
+Set-Location  $Path\src\main\webapp\css
+Dir app.*.css | rename-item -newname app.css
+Dir vendor.*.css | rename-item -newname vendor.css
